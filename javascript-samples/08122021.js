d34001771ed1264c77fe98adde519604e0676cbb
Sinchan Roy <sinchan.roy@real-digital.de>
2021-12-12 10:34:24 +0100

    10122021

--- javascript-samples/08122021.js
+++ javascript-samples/08122021.js
@@ -1,1 +1,10 @@
+
+// This program is for Hello World
+
+// Comment line for alert
+alert("Hello ");
+alert(" World");
+
+//Comment line for writing to a page
 document.write("Hello World")
+
---
+++ javascript-samples/10122021.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// This program is to learn more abot variable
+
+let message = "I got married in "
+let date = 24
+
+const x=5
+
+document.write(message+date);
+document.write(x)
+console.log(x)
